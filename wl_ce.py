aa1667ea5c144ff687a4b07af86bd9ae1ff1084f
ArshadBatman <muhamadarshad20@spe.petroleum.utm.my>
2025-11-13 14:39:00 +0800

    Update wl_ce.py

--- wl_ce.py
+++ wl_ce.py
@@ -230,7 +230,7 @@ if uploaded_file:
                                             "AU2: AUX_PCAL","AU2: AUX_PCAL","PP7: PROC_PETR7","PA7: PROC_ACOU6",
                                             "PA11: PROC_ACOU13","PA12: PROC_ACOU14","IM3: IMAG_SOBM","PI1: PROC_IMAG1",
                                             "PI2: PROC_IMAG2","PI7: PROC_IMAG7"],
-                    "Dual-OBMI DSI (150DegC Max) with  Additional Processing": ["AU14: AUX_SURELOC","GR1: GR_TOTL","AU3: AUX_INCL","AC4: ACOU_ADD1",
+                    "Dual OBMI-Sonic Scanner (150DegC Max) ": ["AU14: AUX_SURELOC","GR1: GR_TOTL","AU3: AUX_INCL","AC4: ACOU_ADD1",
                                             "AU2: AUX_PCAL","AU2: AUX_PCAL","PP7: PROC_PETR7","PA7: PROC_ACOU6",
                                             "PA11: PROC_ACOU13","PA12: PROC_ACOU14","IM3: IMAG_SOBM","PI1: PROC_IMAG1",
                                             "PI2: PROC_IMAG2","PI7: PROC_IMAG7","PI8: PROC_IMAG8","PI9: PROC_IMAG9",
@@ -676,6 +676,7 @@ if st.button("Download Cost Estimate Excel"):
         file_name="Cost_Estimate.xlsx",
         mime="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
     )
+
 
 
 
